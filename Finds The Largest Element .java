46b89d4f519c38cfe1828a19bb8fee8fdf4fda93
leninejoe19 <leninejosewa@gmail.com>
2026-06-08 16:32:56 +0530

    Add files via upload
    Commit

---
+++ Finds The Largest Element .java
@@ -0,0 +1,12 @@
+public class Main {
+public static Void main (string[]args){
+int[] numbers = {10,20,30,40,50}
+int max=numbers[0];
+for (int i = 1;i<numbers.length;i++){
+if (number[i] > max){
+max=number[i];
+}
+}
+System.out.println("Largest Element = " +max);
+}
+}
