aa9684f4ed79839e0b6118c9763e1d0f2600b7f1
leninejoe19 <leninejosewa@gmail.com>
2026-06-12 09:12:48 +0530

    Add files via upload
    commit

---
+++ reverse string.java
@@ -0,0 +1,11 @@
+// reverse string
+public class commonelement {
+    public static void main (String[]args){
+        
+        String a = "Java programming";
+        for (int i = a.length()-1; i>=0; i--){
+            System.out.println(a.charAt(i));
+        }
+        }
+}
+       
